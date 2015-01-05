32b7171a896c9d131d1ab7c8e627366f60e5ccc1
Martin Hosken <martin_hosken@sil.org>
2015-01-05 22:29:43 +0700

    Add triple license from graphite

---
+++ grloader/Application.mk
@@ -0,0 +1,5 @@
+APP_PROJECT_PATH=/home/mhosken/Work/dev/Graphite/grandroid/grloader
+APP_BUILD_SCRIPT=$(APP_PROJECT_PATH)/Android.mk
+APP_PLATFORM=android-15
+APP_ABI=armeabi armeabi-v7a
+APP_MODULES=libgraphite2an libgrload2 libgrload4 libgrload43 libgrload5
